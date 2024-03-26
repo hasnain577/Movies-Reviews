e5befdb709359f80303cda557809d2a81bdef921
Hasnain <162310631+hasnain577@users.noreply.github.com>
2024-03-26 12:46:14 +0500

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-
-import Header from './components/Header';
-import Movie from './components/Movie';
-import Movies from './Movies.json';
-import './App.css';
-
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Movies from "./Movies.json";
+import "./App.css";
 
 function App() {
   let name = "Heading 2";
   return (
-    
     <div className="App">
-        <Header/>
-        <div className='main'>
-     
-           {
-            Movies.map((element)=>{ 
-              return( 
-              <Movie 
-                title={element.Title}
-                year={element.Year}
-                img={element.Poster}
-                />)
-             
-           })
-           }
-  
-        </div>
+      <Header />
+      <div className="main">
+        {Movies.map((element) => {
+          return (
+            <Movie
+              title={element.Title}
+              year={element.Year}
+              img={element.Poster}
+            />
+          );
+        })}
+      </div>
     </div>
-
-  
   );
 }
 
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,14 +1,11 @@
-
-
 const Movie = (props) => {
   return (
     <div className="movie">
-        <img src={props.img} alt="not found" />
-        <p>{props.title}</p>
-        <p>{props.year}</p>
-        
+      <img src={props.img} alt="not found" />
+      <p>{props.title}</p>
+      <p>{props.year}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Movie
+export default Movie;

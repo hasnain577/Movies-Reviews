3aff8e76454fe6f52948f27242bbaa1bb77e68ab
Hasnain <162310631+hasnain577@users.noreply.github.com>
2024-03-26 12:25:07 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import Header from './components/Header';
+import Movie from './components/Movie';
+import Movies from './Movies.json';
+import './App.css';
+
+
+function App() {
+  let name = "Heading 2";
+  return (
+    
+    <div className="App">
+        <Header/>
+        <div className='main'>
+     
+           {
+            Movies.map((element)=>{ 
+              return( 
+              <Movie 
+                title={element.Title}
+                year={element.Year}
+                img={element.Poster}
+                />)
+             
+           })
+           }
+  
+        </div>
+    </div>
+
+  
+  );
+}
+
+export default App;
